d672ca30471e890161f32727109ccf990f581452
Rodrigo A. Hidalgo <72952200+Ralhigar@users.noreply.github.com>
2021-11-05 09:46:44 -0600

    Modificacion graficos

--- models/informeModelo.php
+++ models/informeModelo.php
@@ -21,11 +21,11 @@
         public function reporteAutor($campo,$valor)
         {
             $arreglo = [];
-            $where = ($campo==1)?'a.codigoAutor='.$valor:
+            $where = ($campo==1)?'a.codigoAutor='.$valor:'a.codigoAutor='.$valor;
             $sql = "SELECT a.codigoAutor AS Codigo, a.nombre AS Nombre, l.nombre AS Libro, e.nombre AS Editorial, FORMAT((l.precio + (l.precio*0.13)), 2) AS Precio FROM autor a
             INNER JOIN libro l ON a.codigoLibro = l.codigoLibro
             INNER JOIN editorial e on l.codigoEditorial = e.codigoEditorial
-            WHERE".$where;
+            WHERE ".$where;
             $datos = $this->getDb()->conectar()->query($sql);
             while($fila = $datos->fetch_object()){
                 $arreglo[] = json_decode(json_encode($fila),true);
